ef6f569720dac51b599e38792105b521169b2a9e
Stefan Bajić <stefan@bajic.io>
2023-03-17 23:17:18 +0400

    Fix icons in built app

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 import colors from "vuetify/lib/util/colors";
-import "@mdi/font/css/materialdesignicons.css";
 
 Vue.use(Vuetify);
 
